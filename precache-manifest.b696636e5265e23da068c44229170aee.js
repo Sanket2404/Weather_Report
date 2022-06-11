ddcae719eb3396dee941318b0539671a3c315522
Sanket Prakash Jain <sanketjain242000@gmail.com>
2022-06-11 13:28:39 +0530

    Updates

---
+++ precache-manifest.b696636e5265e23da068c44229170aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b18f095359c4fc8157bc",
+    "url": "/Weather_Report/static/css/main.a24d8c35.chunk.css"
+  },
+  {
+    "revision": "b18f095359c4fc8157bc",
+    "url": "/Weather_Report/static/js/main.a389d056.chunk.js"
+  },
+  {
+    "revision": "67e699ab1fcba39f68b6",
+    "url": "/Weather_Report/static/js/runtime~main.e67d946e.js"
+  },
+  {
+    "revision": "68cb15db6046b592f949",
+    "url": "/Weather_Report/static/js/2.3cc5230f.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/Weather_Report/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "979cf322e2a2b639d79d46f35e39db67",
+    "url": "/Weather_Report/index.html"
+  }
+];
